2bfe223ac86993045eb7ac92339dba9a6a6b921f
b0mb3r <b0mb3r@gmail.com>
2024-12-13 18:56:18 -0300

    alteração toml3

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express"
 import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 
-const PORT = Number(process.env.PORT) || 3333;
+const PORT = Number(process.env.PORT) || 3000;
 const app = express()
 
 app.use(express.json())
